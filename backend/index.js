c4f180ac79b8cfa4b40de49ece952d7435472d32
Sounabho bag <152299664+sounabh@users.noreply.github.com>
2025-02-19 11:50:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 8000;
 app.use(cookieParser())
 // CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://corner-liard.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],

321c57a450ee261fa0a68376d0710ab851ff725a
Sounabho bag <152299664+sounabh@users.noreply.github.com>
2025-02-21 20:46:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8000;
 
 // CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://corner-liard.vercel.app'],
   credentials:true
   
  
